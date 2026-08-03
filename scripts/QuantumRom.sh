aee35ec82a502650b5e7514bf42dfe37c8716ac4
Dai-doz <leduydai181212@gmail.com>
2026-08-03 15:21:04 +0700

    scriptsL fixup!
    Refactor download logic to improve clarity and efficiency.

--- scripts/QuantumRom.sh
+++ scripts/QuantumRom.sh
@@ -174,25 +174,23 @@ DOWNLOAD_FIRMWARE() {
     echo -e "MODEL: $MODEL | CSC: $CSC"
 
     if [ -n "$CUSTOM_URL" ]; then
-        echo -e "📌 Using custom direct download URL: $CUSTOM_URL"
+        echo -e "📌 Detected Direct Link: $CUSTOM_URL"
         
         local FILE_NAME
         FILE_NAME=$(basename "$CUSTOM_URL" | cut -d'?' -f1)
         [ -z "$FILE_NAME" ] && FILE_NAME="firmware.zip"
 
-        local TARGET_PATH="$DOWN_DIR/$FILE_NAME"
-        echo -e "📥 Downloading direct link to $DOWN_DIR..."
-
-        if command -v aria2c >/dev/null 2>&1; then
-            aria2c -x 16 -s 16 -k 1M -d "$DOWN_DIR" -o "$FILE_NAME" "$CUSTOM_URL"
-        elif command -v wget >/dev/null 2>&1; then
-            wget --no-check-certificate -c "$CUSTOM_URL" -O "$TARGET_PATH"
-        else
-            curl -L -k -C - "$CUSTOM_URL" -o "$TARGET_PATH"
-        fi
+        echo -e "📥 Downloading $FILE_NAME to $DOWN_DIR..."
+        
+    if command -v aria2c >/dev/null 2>&1; then
+        aria2c -x 16 -s 16 -k 1M -d "$DOWN_DIR" -o "$FILE_NAME" "$CUSTOM_URL"
+    elif command -v wget >/dev/null 2>&1; then
+        wget --no-check-certificate -c "$CUSTOM_URL" -O "$DOWN_DIR/$FILE_NAME"
+    else
+        curl -L -k -C - "$CUSTOM_URL" -o "$DOWN_DIR/$FILE_NAME"
+    fi
 
     else
-
     if [ -n "$CUSTOM_VERSION" ]; then
         echo -e "📌 Using custom requested version: $CUSTOM_VERSION"
         VERSION="$CUSTOM_VERSION"
