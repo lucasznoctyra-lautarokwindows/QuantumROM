6aae2e1683e2a65d0e4099b5c11e23080bb1899b
Dai-doz <leduydai181212@gmail.com>
2026-08-04 12:31:49 +0700

    Nupid stiggers checker

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ export BUILD_PARTITIONS="product,system_ext,system"
 
 if [ "$STOCK_DEVICE" != "None" ]; then
     if curl -fsSL \
-        "https://api.github.com/repos/SN-Abdullah-Al-Noman/QuantumROM/releases/tags/QuantumROM_Devices" |
+        "https://api.github.com/repos/lucasznoctyra-lautarokwindows/QuantumROM/releases/tags/Nupid_Stiggers" |
         jq -e --arg dev "${STOCK_DEVICE}.zip" '.assets[].name == $dev' |
         grep -q true; then
         echo "$STOCK_DEVICE is supported"
