22b40a0156350c2dae9ef9797f1a32cb24720e07
Dai-doz <leduydai181212@gmail.com>
2026-08-13 14:55:34 +0700

    scripts: missing color bash

---
+++ scripts/bash_colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Recopiled by Lumi
+ESC=$(printf '\033')
+
+# Reset
+RESET="${ESC}[0m"
+
+# Regular Colors
+BLACK="${ESC}[0;30m"
+RED="${ESC}[0;31m"
+GREEN="${ESC}[0;32m"
+YELLOW="${ESC}[0;33m"
+BLUE="${ESC}[0;34m"
+PURPLE="${ESC}[0;35m"
+CYAN="${ESC}[0;36m"
+WHITE="${ESC}[0;37m"
+
+# Bold colors
+BOLD_BLACK="${ESC}[1;30m"
+BOLD_RED="${ESC}[1;31m"
+BOLD_GREEN="${ESC}[1;32m"
+BOLD_YELLOW="${ESC}[1;33m"
+BOLD_BLUE="${ESC}[1;34m"
+BOLD_PURPLE="${ESC}[1;35m"
+BOLD_CYAN="${ESC}[1;36m"
+BOLD_WHITE="${ESC}[1;37m"
+
+# High Intensity colors
+HI_BLACK="${ESC}[0;90m"
+HI_RED="${ESC}[0;91m"
+HI_GREEN="${ESC}[0;92m"
+HI_YELLOW="${ESC}[0;93m"
+HI_BLUE="${ESC}[0;94m"
+HI_PURPLE="${ESC}[0;95m"
+HI_CYAN="${ESC}[0;96m"
+HI_WHITE="${ESC}[0;97m"
